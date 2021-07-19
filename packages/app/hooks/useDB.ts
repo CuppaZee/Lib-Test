fd0d492aadc74612776ec35222ec3ecdde108a26
Sam Hindess <mail@sohcah.dev>
2021-07-19 18:22:43 +0100

    DB Changes

--- packages/app/hooks/useDB.ts
+++ packages/app/hooks/useDB.ts
@@ -2,46 +2,130 @@ import { loadFromCache, loadFromArrayBuffer, loadFromLzwJson, CuppaZeeDB } from 
 import { useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const dbCache: { value: CuppaZeeDB; onLoad: Set<() => void> } = {
+const dbCache: { value: CuppaZeeDB; onLoad: Set<() => void>; running: boolean } = {
   value: new CuppaZeeDB([], [], []),
   onLoad: new Set(),
+  running: false,
 };
 
-(async function () {
-  const cacheData = await AsyncStorage.getItem("@czexpress/dbcache");
-  let cacheVersion = 0;
-  if (cacheData) {
-    const data = JSON.parse(cacheData);
-    cacheVersion = data.version;
-    dbCache.value = loadFromCache(data);
-  }
-  try {
-    const response = await fetch(`https://db.cuppazee.app/lzwmsgpack/${cacheVersion}`);
+export const dbLoadLog: string[] = [];
+
+async function loadDB2(cacheVersion: number) {
+  try{
+    const response = await Promise.race([
+      await fetch(`https://db.cuppazee.app/lzw/${cacheVersion}`),
+      new Promise<never>((_, r) => setTimeout(r, 3000)),
+    ]);
     if (!response.ok) throw "e";
-    const data = await response.arrayBuffer();
-    if (data.byteLength > 0) {
-      const { db, cache } = loadFromArrayBuffer(data);
+    dbLoadLog.push("Loaded from lzw, Reading.");
+    const data = await Promise.race([
+      await response.text(),
+      new Promise<never>((_, r) => setTimeout(r, 500)),
+    ]);
+    if (data.length > 0) {
+      dbLoadLog.push("Read from lzw, Parsing.");
+      const { db, cache } = loadFromLzwJson(data);
+      dbLoadLog.push("Parsed from lzw, Succeeded.");
       dbCache.value = db;
+      dbCache.onLoad.forEach(i => i());
       await AsyncStorage.setItem("@czexpress/dbcache", JSON.stringify(cache));
+    } else {
+      dbLoadLog.push("Nothing to load from lzw, Suspended.");
     }
   } catch (e) {
-    const response = await fetch(`https://db.cuppazee.app/lzw/${cacheVersion}`);
-    if (!response.ok) throw "e";
-    const data = await response.text();
-    if (data.length > 0) {
-      const { db, cache } = loadFromLzwJson(data);
-      dbCache.value = db;
-      await AsyncStorage.setItem("@czexpress/dbcache", JSON.stringify(cache));
-    }
+    dbLoadLog.push("Failed to load from lzw, Failed.");
   }
-})();
+}
+
+async function loadDB(cacheVersion: number, depth: number = 0) {
+  if (depth > 5) {
+    dbLoadLog.push("Depth 5 Reached, Suspended.");
+    return null;
+  }
+  try {
+    const response = await Promise.race([
+      await fetch(`https://db.cuppazee.app/lzwmsgpack/${cacheVersion}`),
+      new Promise<never>((_, r) => setTimeout(r, 1000)),
+    ]);
+    if (!response.ok) throw "e";
+    dbLoadLog.push("Loaded from lzwmsgpack, Reading.");
+    const data = await Promise.race([
+      await response.arrayBuffer(),
+      new Promise<never>((_, r) => setTimeout(r, 500)),
+    ]);
+    if (data.byteLength > 0) {
+      dbLoadLog.push("Read from lzwmsgpack, Parsing.");
+      const { db, cache } = loadFromArrayBuffer(data);
+      dbLoadLog.push("Parsed from lzwmsgpack, Succeeded.");
+      dbCache.value = db;
+      dbCache.onLoad.forEach(i => i());
+      await AsyncStorage.setItem("@czexpress/dbcache", JSON.stringify(cache));
+    } else {
+      dbLoadLog.push("Nothing to load from lzwmsgpack, Suspended.");
+    }
+  } catch (e) {
+    dbLoadLog.push("Failed to load from lzwmsgpack, Continuing.");
+    try {
+      const response = await Promise.race([
+        await fetch(`https://db.cuppazee.app/lzw/${cacheVersion}`),
+        new Promise<never>((_, r) => setTimeout(r, 1000)),
+      ]);
+        if (!response.ok) throw "e";
+        dbLoadLog.push("Loaded from lzw, Reading.");
+        const data = await Promise.race([
+          await response.text(),
+          new Promise<never>((_, r) => setTimeout(r, 500)),
+        ]);
+        if (data.length > 0) {
+          dbLoadLog.push("Read from lzw, Parsing.");
+          const { db, cache } = loadFromLzwJson(data);
+          dbLoadLog.push("Parsed from lzw, Succeeded.");
+          dbCache.value = db;
+          dbCache.onLoad.forEach(i => i());
+          await AsyncStorage.setItem("@czexpress/dbcache", JSON.stringify(cache));
+        } else {
+          dbLoadLog.push("Nothing to load from lzw, Suspended.");
+        }
+      } catch (e) {
+        dbLoadLog.push("Failed to load from lzw/lzwmsgpack, Continuing.");
+        await new Promise(r => setTimeout(r, 5000));
+        await loadDB(cacheVersion, depth + 1);
+      }
+  };
+  dbLoadLog.push("Finished.");
+}
+
+async function loadDBBase() {
+  dbCache.running = true;
+  dbLoadLog.push("Loading...");
+  let cacheVersion = 0;
+  try {
+    const cacheData = await Promise.race([AsyncStorage.getItem("@czexpress/dbcache"), new Promise<string | null>((_, r) => setTimeout(r, 1000))]);
+    if (cacheData) {
+      dbLoadLog.push("Loaded from cache, Continuing.");
+      const data = JSON.parse(cacheData);
+      cacheVersion = data.version;
+      dbCache.value = loadFromCache(data);
+      dbCache.onLoad.forEach(i => i());
+    } else {
+      dbLoadLog.push("No Cache Data, Continuing.")
+    }
+  } catch (e) { dbLoadLog.push("Failed to load from cache, Continuing.");}
+  await loadDB2(cacheVersion);
+};
 
 export default function useDB() {
   const [_, setValue] = useState(0);
   useEffect(() => {
+    if (!dbCache.running) {
+      loadDBBase();
+    }
     const f = () => {
       setValue(i => i + 1);
     };
+    if (dbCache.value) {
+      f()
+    }
     dbCache.onLoad.add(f);
     return () => { dbCache.onLoad.delete(f) };
   }, []);
--- packages/app/navigation-drawer/Drawer.tsx
+++ packages/app/navigation-drawer/Drawer.tsx
@@ -6,9 +6,10 @@ import {
   DrawerItem as UIKittenDrawerItem,
   DrawerItemProps,
   Layout,
+  Text,
 } from "@ui-kitten/components";
-import React from "react";
-import { Image, Platform, ScrollView, useWindowDimensions, View } from "react-native";
+import React, { useState } from "react";
+import { Alert, Image, Platform, ScrollView, useWindowDimensions, View } from "react-native";
 import { useClanBookmarks, useUserBookmarks } from "../hooks/useBookmarks";
 import useDay from "../hooks/useDay";
 import { useTranslation } from "react-i18next";
@@ -16,6 +17,8 @@ import Tip from "../components/Common/Tip";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import Icon from "../components/Common/Icon";
 import useSetting, { DrawerAtom, ReadyAtom } from "../hooks/useSetting";
+import ExpoClipboard from "expo-clipboard";
+import useDB, { dbLoadLog } from "../hooks/useDB";
 
 const DrawerItem = React.memo(
   function ({ title, style, ...props }: DrawerItemProps) {
@@ -71,6 +74,8 @@ const MainDrawerContent = function ({ page, navigation }: { page: any[]; navigat
   const day = useDay();
   const [drawerSettings, setDrawerSettings] = useSetting(DrawerAtom);
   const dimensions = useWindowDimensions();
+  const [showDBDebug, setDBDebug] = useState(false);
+  useDB();
   const open = drawerSettings.open || dimensions.width <= 1000;
   return (
     <Layout style={{ flex: 1 }}>
@@ -536,6 +541,30 @@ const MainDrawerContent = function ({ page, navigation }: { page: any[]; navigat
               screen: "Donate",
             })
           }
+        />
+        {showDBDebug && (
+          <>
+            <Text category="h6">Hello 2</Text>
+            {dbLoadLog.map(i => (
+              <View>
+                <Text category="s1">{i}</Text>
+              </View>
+            ))}
+          </>
+        )}
+        <DrawerItem
+          selected={false}
+          title="View DB Debug"
+          accessoryLeft={props => <Icon {...props} name="star" />}
+          onPress={() => {
+            // ExpoClipboard.setString(dbLoadLog.join(', \n'))
+            setDBDebug(true);
+            // if (Platform.OS === "web") {
+            //   alert("Copied to Clipboard")
+            // } else {
+            //   Alert.alert("Copied to Clipboard");
+            // }
+          }}
         />
         {dimensions.width > 1000 && <View style={{ height: 44 }} />}
       </ScrollView>
