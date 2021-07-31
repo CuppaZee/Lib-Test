9ad0d85e162cb8d275cf44b3e2b5e79e0604b397
Sam Hindess <mail@sohcah.dev>
2021-07-31 16:07:59 +0000

    New Bouncers Endpoint

---
+++ packages/api/munzee/bouncers.d.ts
@@ -0,0 +1,32 @@
+import { Endpoint, Response } from "../common";
+
+export interface MunzeeBouncer {
+  munzee_id: string;
+  latitude: string;
+  longitude: string;
+  friendly_name: string;
+  time_placed: string;
+  full_url: string;
+  mythological_munzee: {
+    friendly_name: string;
+    code: string;
+    creator_user_id: string;
+    creator_username: string;
+    munzee_id: string;
+    munzee_logo: string;
+    capture_type_id: string;
+  },
+  special_good_until: number;
+}
+
+export type MunzeeBouncerGroup = "mythological" | "pouch_creature" | "flat" | "evo" | "garden_gnome" | "mechz" | "clan" | "temp" | "retired" | "tob";
+
+export interface MunzeeBouncers extends Endpoint {
+  request: {
+    endpoint: "munzee/bouncers";
+    params: {
+      group: MunzeeBouncerGroup
+    };
+  }
+  response: Response<MunzeeBouncer[]>
+}
