c4fe6cfb3bfd9086748bed9197ddfe119ef9bf7f
Sam Hindess <mail@sohcah.dev>
2021-09-21 15:24:55 +0100

    Move App Folder

--- packages/app/react-i18next.d.ts
+++
@@ -1,6 +0,0 @@
-import {TranslationFormat} from "./lang/data";
-
-declare module "react-i18next" {
-  type DefaultResources = TranslationFormat;
-  interface Resources extends DefaultResources {}
-}
--- packages/app/screens/Tools/WidgetConfigure/SharedStorage.ts
+++
@@ -1,7 +0,0 @@
-import { NativeModules } from "react-native";
-const { SharedStorage } = NativeModules;
-interface SharedStorageInterface {
-  set(key: string, value: string): void;
-  get(key: string, defaultValue: string): Promise<string>;
-}
-export default SharedStorage as SharedStorageInterface;
